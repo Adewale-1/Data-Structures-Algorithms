d515b625b9fbacfb21a31cac53ec06d785396ad4
Adewale-Adenle <56052969+Adewale-1@users.noreply.github.com>
2023-07-06 21:54:39 -0400

    Update

--- DSA.java
+++ DSA.java
@@ -3,12 +3,13 @@ public class DSA {
     public static void main(String[] args) {
         int[] arr = { 9, 3, 5 };
         bubbleSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.println(arr[i]);
+        // }
+        int value = binarySearch(arr, 5);
+        System.out.println("Value found at index: " + value);
+
     }
-//
-// {4,2,5,2,5,9,3,1}
 
     public static void bubbleSort(int array[]) {
         for (int i = 0; i < array.length - 1; i++) {
@@ -20,5 +21,36 @@ public class DSA {
                 }
             }
         }
+    }
+
+    // {2,3,5}
+    // value = 5
+    // high = 2
+    // low = 0
+    // middle = 1
+    // target = 2
+
+    public static int binarySearch(int array[], int value) {
+        int high = array.length - 1;
+        int low = 0;
+        int index = 0;
+
+        while (low <= high) {
+            int middle = low + (low + high) / 2;
+            int target = array[middle];
+
+            if (value < target) {
+                // Too high
+                high = middle + 1;
+            } else if (value > target) {
+                // Too Low
+                low = middle - 1;
+            } else {
+                // Value found
+                index = array[middle];
+            }
+        }
+
+        return index;
     }
 }
