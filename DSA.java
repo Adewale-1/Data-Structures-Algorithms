6478221ebb4886fca76ef7a5b222585de09d202c
Adewale-Adenle <56052969+Adewale-1@users.noreply.github.com>
2023-07-10 23:41:48 -0400

    update

--- DSA.java
+++ DSA.java
@@ -2,22 +2,22 @@ import javax.print.attribute.SetOfIntegerSyntax;
 
 public class DSA {
     public static void main(String[] args) {
-        int[] arr = { 3, 9, 2, 1, 6,34,5,54,76,72,436,475,42,265,65,6764,3234,9054,335 ,35};
-        bubbleSort(arr);
+        int[] arr = { 3, 9, 2, 1, 6, 34, 5, 54, 76, 72, 436, 475, 42, 265, 65, 6764, 3234, 9054, 335, 35 };
+        InsertionSort(arr);
         // int value2 = binarySearch(arr, 475);
         // int value2 = LinearSearch(arr, 475);
-        int value2 = InterpolationSearch(arr, 475);
+        // int value2 = InterpolationSearch(arr, 475);
         // SelectionSort(arr);
-        // for (int i = 0; i < arr.length; i++) {
-        //     System.out.print(arr[i]);
-        // }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
 
-        if (value2 != -1){
-            System.out.println("Value found at index: " + value2);
-        }
-        else{
-            System.out.println("Value not found");
-        }
+        // if (value2 != -1){
+        // System.out.println("Value found at index: " + value2);
+        // }
+        // else{
+        // System.out.println("Value not found");
+        // }
     }
 
     // Sort Algorithms
@@ -64,23 +64,46 @@ public class DSA {
         }
     }
 
-    public static void InsertionSort() {
+    // After computing elements to the left, shift elements to the right to make room to insert a value
+        // Runtime complexity of O(n^2)
+        // Not good for large datasets
+        // Best case: O(n) compared to Selection sort O(n^2)
+    /**
+     * @param
+     * array
+     * @requires
+     *           array is an array
+     * @ensures
+     *          array is sorted
+     */   
+    public static void InsertionSort(int array[]) {
 
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
     }
 
     private static void MergeSort() {
     }
 
     // Search Algorithms
-    // This method searches for the position of a target within a sorted array. Half of the array was elimated during each step
-                            // Runtime complexity : O(log n)
+    // This method searches for the position of a target within a sorted array. Half
+    // of the array was elimated during each step
+    // Runtime complexity : O(log n)
 
-                            // Disadvantages:
-                            // Not suitable for large data Sets
+    // Disadvantages:
+    // Not suitable for large data Sets
 
-                            // Advantages:
-                            // Fast for small data Sets
-                            // Suitable for data structures that do not have random access
+    // Advantages:
+    // Fast for small data Sets
+    // Suitable for data structures that do not have random access
     /**
      * @param
      * array,        integer
@@ -112,16 +135,16 @@ public class DSA {
         return index;
     }
 
-    //  Iterate through the array one element at a time
+    // Iterate through the array one element at a time
 
-                            // Runtime complexity : O(n)
+    // Runtime complexity : O(n)
 
-                            // Disadvantages:
-                            // Not suitable for large data Sets
+    // Disadvantages:
+    // Not suitable for large data Sets
 
-                            // Advantages:
-                            // Fast for small data Sets
-                            // Suitable for data structures that do not have random access
+    // Advantages:
+    // Fast for small data Sets
+    // Suitable for data structures that do not have random access
     /**
      * @param
      * array,        integer
@@ -142,19 +165,19 @@ public class DSA {
         return index;
     }
 
-    //  Improvement over binary search for uniformly distributed.
-    //  It guesses where a value might be based on calculated probe results
-    //  If probe is incorrect, search area is narrowed, and the probe is calculated
+    // Improvement over binary search for uniformly distributed.
+    // It guesses where a value might be based on calculated probe results
+    // If probe is incorrect, search area is narrowed, and the probe is calculated
 
-                            // Runtime complexity : O(Log(Log n))
-                            // Worst case: 0(n)
+    // Runtime complexity : O(Log(Log n))
+    // Worst case: 0(n)
 
-                            // Disadvantages:
-                            // Not suitable for large data Sets
+    // Disadvantages:
+    // Not suitable for large data Sets
 
-                            // Advantages:
-                            // Fast for small data Sets
-                            // Suitable for data structures that do not have random access
+    // Advantages:
+    // Fast for small data Sets
+    // Suitable for data structures that do not have random access
     /**
      * @param
      * array,        integer
@@ -163,20 +186,18 @@ public class DSA {
      * @ensures
      *          index of terget in array
      */
-    public static int InterpolationSearch(int array[], int target){
-        int high = array.length -1;
+    public static int InterpolationSearch(int array[], int target) {
+        int high = array.length - 1;
         int low = 0;
         int index = -1;
-        while (target >= array[low] && target <= array[high] && low <= high && index == -1){
-            int probe = low + (high - low) * (target - array[low])/ (array[high] - array[low]);
-            
-            if (array[probe] == target){
+        while (target >= array[low] && target <= array[high] && low <= high && index == -1) {
+            int probe = low + (high - low) * (target - array[low]) / (array[high] - array[low]);
+
+            if (array[probe] == target) {
                 index = probe;
-            }
-            else if (array[probe] < target){
+            } else if (array[probe] < target) {
                 low = probe + 1;
-            }
-            else {
+            } else {
                 high = probe - 1;
             }
         }
@@ -184,4 +205,3 @@ public class DSA {
     }
 
 }
-
