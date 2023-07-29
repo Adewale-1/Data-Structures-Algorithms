ba0f5c80b950eb1a5aba981a5d99a7aaf58c054f
Adewale-Adenle <56052969+Adewale-1@users.noreply.github.com>
2023-07-29 19:16:34 -0400

    update

--- LL.java
+++ LL.java
@@ -13,6 +13,11 @@ public class LL {
         this.size = 0;
     }
 
+    public int size(){
+        return size;
+    }
+
+
     public void insertFirst(int value) {
         Node node = new Node(value);
         node.next = head;
@@ -29,11 +34,100 @@ public class LL {
     public void display(){
         Node temp = head;
         while (temp != null){
-            System.out.print(temp.value + "->");
+            System.out.print(temp.value + " -> ");
             temp = temp.next;
         }
         System.out.print("null");
     }
+
+    public void insertLast (int value){
+        if (tail == null){
+            insertFirst(value);
+        }
+
+        Node node = new Node(value);
+
+        tail.next = node;
+        node.next = null;
+        tail = node;
+        
+        size++;
+
+    }
+    public void insert(int index, int value){
+        Node node = new Node(value);
+
+        if (index == 0){
+            insertFirst(value);
+        }
+        else if (index == size){
+            insertLast(value);
+        }
+        else{
+            Node temp = head;
+            Node after = head;
+            for (int i = 1; i < index; i++) {
+                temp  = temp.next;
+            }
+            for (int i = 1; i < index + 1; i++) {
+                after = after.next;
+            }
+            temp.next = node;
+            node.next = after;
+        }
+        size++;
+    }
+
+    public void deleteFirst(){
+
+        if (head.next == null){
+            head = null;
+            tail = null;
+        }
+        else{
+            Node node = head.next;
+            head = node;
+        }
+        size--;
+
+    }
+
+    public void deleteLast(){
+        if (head.next == null){
+            deleteFirst();
+        }
+        else{
+            Node temp = head;
+            for (int i = 1; i < size - 1; i++) {
+                temp = temp.next;
+            }
+            
+            tail = temp;
+            tail.next = null;
+        }
+    }
+
+    public void delete(int index){
+        if (index == 0){
+            deleteFirst();
+        }
+        else if (index == size - 1){
+            deleteLast();
+       }
+       else{
+            Node temp = head;
+            for (int i = 1; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            Node after = head;
+            for (int i = 1; i < index + 1; i++) {
+                after = temp.next;
+            }
+
+            temp.next = after;        
+       }
+    }
+
 
     private class Node{ 
         private int value;
--- main.java
+++ main.java
@@ -2,9 +2,19 @@ public class main {
     public static void main(String[] args) {
         
         LL list = new LL();
+
         list.insertFirst(3);
-        // list.insertFirst(4);
-        // list.insertFirst(8);
+
+        list.insertFirst(4);
+
+        list.insertFirst(8);
+
+        list.insertLast(9);
+
+        list.insert(2, 2);
+
+        System.out.println(list.size());
+
         list.display();
     }
     
